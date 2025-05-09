f3d972ef941fec51adf69c728be59e770ddba956
BeechPPC <chris.beecheyppc@gmail.com>
2025-05-09 15:50:52 +1000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import SearchTermTable from './components/SearchTermTable';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Ads Search Term Review</h1>
+      <SearchTermTable />
+    </div>
+  );
+}
+
+export default App;
 
