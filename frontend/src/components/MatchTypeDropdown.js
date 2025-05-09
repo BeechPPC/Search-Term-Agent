00b929e2deb875fcfa2c5f9488287dab37a6c72e
BeechPPC <chris.beecheyppc@gmail.com>
2025-05-09 15:51:50 +1000

    Update MatchTypeDropdown.js

--- frontend/src/components/MatchTypeDropdown.js
+++ frontend/src/components/MatchTypeDropdown.js
@@ -1,1 +1,10 @@
 
+export default function MatchTypeDropdown({ onChange }) {
+  return (
+    <select onChange={(e) => onChange(e.target.value)}>
+      <option value="phrase">Phrase</option>
+      <option value="exact">Exact</option>
+      <option value="broad">Broad</option>
+    </select>
+  );
+}
