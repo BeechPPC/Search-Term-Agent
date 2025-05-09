976ffe1df85208a07a23a1f2c8296cccbf245036
BeechPPC <chris.beecheyppc@gmail.com>
2025-05-09 15:52:03 +1000

    Update NegListDropdown.js

--- frontend/src/components/NegListDropdown.js
+++ frontend/src/components/NegListDropdown.js
@@ -1,1 +1,13 @@
+export default function NegListDropdown({ lists, onChange }) {
+  return (
+    <select onChange={(e) => onChange(e.target.value)}>
+      <option value="">Select List</option>
+      {lists.map(list => (
+        <option key={list.id} value={list.id}>
+          {list.name} ({list.keywordCount})
+        </option>
+      ))}
+    </select>
+  );
+}
 
